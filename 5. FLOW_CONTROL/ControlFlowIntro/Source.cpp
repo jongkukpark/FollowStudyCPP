8130cc28437b3e3f1258e9bb92342b5fdc0c9306
jongkukpark <jongkukpark162@cnu.ac.kr>
2019-01-31 09:53:43 +0900

    5.1 제어 흐름 개요 Control flow

---
+++ 5. FLOW_CONTROL/ControlFlowIntro/Source.cpp
@@ -0,0 +1,17 @@
+/*
+	Halt: 중단
+	Jump: 점프
+	Conditional branches: 조건 분기
+	Loops: 반복
+	Exceptions: 예외 처리
+*/
+
+#include <iostream>
+#include <cstdlib>
+
+int main()
+{
+	std::cout << "I love you " << std::endl;
+
+	exit(0);
+}
