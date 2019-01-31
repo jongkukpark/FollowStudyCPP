5f9786c5d4abee64e8a5de5acd68f758e9c2f9ab
jongkukpark <jongkukpark162@cnu.ac.kr>
2019-01-31 19:21:49 +0900

    5.2 조건문 if

---
+++ 5. FLOW_CONTROL/If/Source.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+int main()
+{
+	using namespace std;
+
+	int x;
+	cin >> x;
+
+	if (x > 10)
+	{
+		cout << x << " is greater than 10" << endl;
+	}
+	else
+	{
+		cout << x << " is not greater than 10" << endl;
+	}
+
+
+	return 0;
+}
