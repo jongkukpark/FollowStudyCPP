3f0114c904e202aaa4a866ee01ebd3dcbfe13a07
jongkukpark <jongkukpark162@cnu.ac.kr>
2019-02-06 18:01:57 +0900

    5.6 반복문 do-while

---
+++ 5. FLOW_CONTROL/DoWhile/Source.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int selection;
+
+	do
+	{
+		cout << "1 add" << endl;
+		cout << "2 sub" << endl;
+		cout << "3 mult" << endl;
+		cout << "4 DIV" << endl;
+		cin >> selection;
+	} while (selection <= 0 || selection >= 5);
+
+	cout << "You selected " << selection << endl;
+
+	return 0;
+}
